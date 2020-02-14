f835d1ce6656567b833d86704d505d4fcbad0708
Dillon Williams <dillonwilliams@fastmail.com>
2020-02-13 18:12:29 -0600

    Export DcmtkDicomClient

--- pacsman/__init__.py
+++ pacsman/__init__.py
@@ -1,4 +1,5 @@
 from .base_client import BaseDicomClient # noqa
 from .pynetdicom_client import PynetDicomClient # noqa
+from .dcmtk_client import DcmtkDicomClient # noqa
 from .filesystem_dev_client import FilesystemDicomClient # noqa
 from .utils import dataset_attribute_fetcher, copy_dicom_attributes, dicom_file_iterator # noqa
