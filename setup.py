bbb684ef268b04c5016ff3643478d3b49b8f49e6
Reece Stevens <reecestevens24@gmail.com>
2023-06-15 09:58:46 -0500

    Bump version to 0.1.8

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ description = 'pacsman: Picture Archiving and Communication System Manager And N
 
 setup(
     name='pacsman',
-    version='0.1.7',
+    version='0.1.8',
     description=description,
     long_description=description,
     url='https://github.com/innolitics/pacsman',
